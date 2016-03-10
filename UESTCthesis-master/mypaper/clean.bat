28db549bc292278031e447a0aa16ccd7792e6e75
martian9 <645342007@qq.com>
2016-03-10 16:13:06 +0800

    mod

--- UESTCthesis-master/mypaper/clean.bat
+++ UESTCthesis-master/mypaper/clean.bat
@@ -6,7 +6,7 @@ del /S *.blg 2>nul
 del /S *.out 2>nul
 del /S *.thm 2>nul
 del /S *.toc 2>nul
-del /S *.synctex* 2>nul
+rem del /S *.synctex* 2>nul
 del /S *.glg 2>nul
 del /S *.glo 2>nul
 del /S *.gls 2>nul
--- UESTCthesis-master/mypaper/compile.bat
+++ UESTCthesis-master/mypaper/compile.bat
@@ -7,15 +7,15 @@ echo 请关闭打开的%jobname%.pdf再重新执行。
 pause
 exit
 ) 
-pdflatex .\%jobname%.tex
+pdflatex -synctex=-1 .\%jobname%.tex
 makeindex -s %jobname%.ist -t %jobname%.glg -o %jobname%.gls %jobname%.glo
 makeindex -s %jobname%.ist -t %jobname%.alg -o %jobname%.acr %jobname%.acn
 bibtex .\%jobname%.aux
 bibtex .\publications.aux
-pdflatex .\%jobname%.tex
+pdflatex -synctex=-1 .\%jobname%.tex
 makeindex -s %jobname%.ist -t %jobname%.glg -o %jobname%.gls %jobname%.glo
 makeindex -s %jobname%.ist -t %jobname%.alg -o %jobname%.acr %jobname%.acn
-pdflatex .\%jobname%.tex
+pdflatex -synctex=-1 .\%jobname%.tex
 call clean.bat
 cls
 echo ****************************************************************************
